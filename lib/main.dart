8b2e3f748873a7daa19835c7c0082c711deb0c42
menna <mennakhalood01@gmail.com>
2024-08-04 18:53:12 +0300

    Updated main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/home/home_screen.dart';
-
-void main (){
-  runApp(MyApp());
+import 'package:todo_app/my_theme_data.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:todo_app/providers/list_provider.dart';
+void main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid ?
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyC_9Pe2QQnsC8y7ayLqptia9LAbi0jN9e8",
+        appId: "com.example.todo_app",
+        messagingSenderId: "896910991119",
+        projectId: "todo-app-c1b2a"
+    )
+  )
+      :
+  await Firebase.initializeApp();
+  await FirebaseFirestore.instance.disableNetwork();
+  runApp(ChangeNotifierProvider(
+      create: (context) => ListProvider(),
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget{
@@ -15,6 +35,7 @@ class MyApp extends StatelessWidget{
         HomeScreen.routeName : (context) => HomeScreen(),
 
       },
+      theme: MyThemeData.LightTheme,
     );
   }
 }
